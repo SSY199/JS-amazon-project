03ddfaf91671584bbaa154e89be8528b1031efb1
Yadav Sahil <s.s.y.9321423@gmail.com>
2024-07-21 13:50:50 +0530

    Make the cartquantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,6 +82,16 @@ document.querySelector('.js-products-grid')
             quantity: 1
           });
         }
-        console.log(cart);
+
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+         .innerHTML = cartQuantity;
+
+     
       });
     });
